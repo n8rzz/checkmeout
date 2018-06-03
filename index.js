28d7f62ef2cd2c8383353300a7b0b7c01dc46eeb
Nate Geslin <ngeslin@nerdery.com>
2018-06-03 14:53:51 -0500

    moves files back to index.js * removes lib dir * adds @n8rzz/branches to abstract away git branch parsing

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-require('./lib/checkmeout');
+const { prompt } = require('inquirer');
+const chalk = require('chalk');
+const branch = require('@n8rzz/branches');
+
+(function () {
+    const branchList = branch(_buildCheckboxList);
+
+    function _buildCheckboxList(branchList) {
+        const questions = [
+            {
+                name: 'branchToCheckout',
+                type: 'list',
+                message: 'Select a branch to checkout',
+                choices: branchList
+            }
+        ];
+
+        prompt(questions)
+            .then((answers) => {
+                initiateBranchChange(answers.branchToCheckout);
+            });
+    }
+
+    function initiateBranchChange(branchToCheckout) {
+        try {
+            _checkoutBranch(branchToCheckout);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    function _checkoutBranch(branchName) {
+        exec(`git checkout ${branchName}`, (err, stdout, stderr) => {
+            if (err) {
+                throw err;
+            }
+        });
+    }
+})()
