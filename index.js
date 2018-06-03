5493f843d3f85e1703c940d9ab66696c55311ed4
Nate Geslin <ngeslin@nerdery.com>
2018-06-03 15:15:00 -0500

    completes integration of `@n8rzz/branches` and updates package refs

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 const { prompt } = require('inquirer');
 const chalk = require('chalk');
-const branch = require('@n8rzz/branches');
+const branches = require('@n8rzz/branches');
+
+function _buildCheckboxList(branchList) {
+    const questions = [
+        {
+            name: 'branchToCheckout',
+            type: 'list',
+            message: 'Select a branch to checkout',
+            choices: branchList
+        }
+    ];
+
+    prompt(questions)
+        .then((answers) => {
+            initiateBranchChange(answers.branchToCheckout);
+        });
+}
+
+function initiateBranchChange(branchToCheckout) {
+    try {
+        _checkoutBranch(branchToCheckout);
+    } catch (error) {
+        throw error;
+    }
+}
+
+function _checkoutBranch(branchName) {
+    exec(`git checkout ${branchName}`, (err, stdout, stderr) => {
+        if (err) {
+            throw err;
+        }
+    });
+}
 
 (function () {
-    const branchList = branch(_buildCheckboxList);
-
-    function _buildCheckboxList(branchList) {
-        const questions = [
-            {
-                name: 'branchToCheckout',
-                type: 'list',
-                message: 'Select a branch to checkout',
-                choices: branchList
-            }
-        ];
-
-        prompt(questions)
-            .then((answers) => {
-                initiateBranchChange(answers.branchToCheckout);
-            });
-    }
-
-    function initiateBranchChange(branchToCheckout) {
-        try {
-            _checkoutBranch(branchToCheckout);
-        } catch (error) {
-            throw error;
-        }
-    }
-
-    function _checkoutBranch(branchName) {
-        exec(`git checkout ${branchName}`, (err, stdout, stderr) => {
-            if (err) {
-                throw err;
-            }
-        });
-    }
+    const branchList = branches().then((branchList) => _buildCheckboxList(branchList));
 })()
